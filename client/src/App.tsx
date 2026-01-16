21bedcbb4b13d25d33a3729f9906edbcb22f059e
Jennifer Khamdaraphone <jkhamdaraphone@gmail.com>
2026-01-16 12:22:31 -0600

    Search bar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { useState } from 'react'
 import Home from "./pages/Home.tsx";
 import AnalyticsBreakdown from "./pages/AnalyticsBreakdown.tsx";
 import MatchHistory from "./pages/MatchHistory.tsx";
+import SearchBar from "./components/SearchBar.tsx";
+import Series from "./components/Series.tsx";
 
 function App() {
+  const [searchedTeam, setSearchedTeam] = useState<string>("");
+
+  const handleSearch = (searchValue: string) => {
+    console.log("Searching for:", searchValue);
+    setSearchedTeam(searchValue);
+  };
 
   return (
     <BrowserRouter>
@@ -13,6 +22,10 @@ function App() {
             <Link to="/analytics">Analytics</Link>
             <Link to="/history">Match History</Link>
         </nav>
+
+        <SearchBar onSearch={handleSearch} />
+
+        {searchedTeam && <Series seriesId="test" selectedTeam={searchedTeam} />}
 
         <Routes>
             <Route path="/" element={<Home />} />
